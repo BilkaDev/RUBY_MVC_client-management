37bb215d6ebe1f2d937fba204a9168865cfd06d3
Marcin Biłka <bilkadev@gmail.com>
2023-03-16 22:11:23 +0100

    added js change datetime in sidebar.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+
+
+setInterval(() => {
+    const pTime = document.querySelector('#sidebar-time')
+
+    const minute = new Date().getMinutes()
+    const hour = new Date().getHours()
+    pTime.textContent = `${hour}:${minute}`
+
+}, 10000)
